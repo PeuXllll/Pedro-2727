40f75be667c71f2855b1987ac6872799abf51b43
PeuXllll <77284492+PeuXllll@users.noreply.github.com>
2021-02-18 04:03:17 -0300

    Update badut.sh

--- badut.sh
+++ badut.sh
@@ -73,9 +73,6 @@ clear
 sleep 2
 echo "          $merah   instalando o segundo login"
 sleep 2
-git clone https://github.com/Darkziinh/SITEX
-git clone https://github.com/Darkziinh/apikey
-git clone https://github.com/Darkziinh/testek
 cd darkbot3.0
 chmod +x git.sh
 echo "$verde"
